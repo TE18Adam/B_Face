8b2f40d00018b5d35dce1a727335068fae4ecd42
adam zbati <adam.zbati2@gmail.com>
2021-02-23 13:31:11 +0100

    first commit

---
+++ API övning/API.cs
@@ -0,0 +1,14 @@
+using System;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace API
+{
+    public class API
+    {
+        public string name;
+        public string eye_colour;
+        public string gender;
+
+    }
+}
---
+++ API övning/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace API_övning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            RestClient client = new RestClient("https://swapi.dev/api/");
+            
+            
+            RestRequest  request= new RestRequest("https://swapi.dev/api/people/1/");
+
+
+            IRestResponse response = client.Get(request);
+
+            
+            Program person = JsonConvert.DeserializeObject<Program>(response.Content); 
+            
+            
+            Console.WriteLine(response.Content);
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
